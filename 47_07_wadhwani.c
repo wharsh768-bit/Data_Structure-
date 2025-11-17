3fceaab43e014a8311ef87e96ebd4d4f506b3ab9
wharsh768-bit <wharsh768@gmail.com>
2025-11-17 23:17:47 +0530

    Create 47_07_wadhwani.c

---
+++ 47_07_wadhwani.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define SIZE 100
+
+char stack[SIZE];
+int top = -1;
+
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int isEmpty() {
+    return top == -1;
+}
+
+int precedence(char ch) {
+    if (ch == '^') return 3;
+    if (ch == '*' || ch == '/') return 2;
+    if (ch == '+' || ch == '-') return 1;
+    return 0;
+}
+
+int isOperator(char ch) {
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+}
+
+void convertToPostfix(char *infix, char *postfix) {
+    int i = 0, j = 0;
+    char ch;
+
+    while ((ch = infix[i++]) != '\0') {
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        } else if (ch == '(') {
+            push(ch);
+        } else if (ch == ')') {
+            while (!isEmpty() && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            if (!isEmpty()) pop(); 
+        } else if (isOperator(ch)) {
+            while (!isEmpty() && precedence(peek()) >= precedence(ch)) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while (!isEmpty()) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[SIZE], postfix[SIZE];
+
+    printf("Enter an infix expression: ");
+    scanf("%s", infix);
+
+    convertToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
